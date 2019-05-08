8dde81ff4b6576cb05e49ca21c456fe027c3f398
Nicklas Schioldan Winther <nick3845@edu.campusvejle.dk>
2019-05-08 11:53:34 +0200

    You can now see all renters, add renters and search for renters based on name

---
+++ ItRental.Dal/RenterRepository.cs
@@ -0,0 +1,48 @@
+﻿using ItRental.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ItRental.Dal
+{
+    public class RenterRepository : BaseRepository
+    {
+        public List<Renter> GetRenters()
+        {
+            string sql = $"SELECT * FROM Renters";
+            return HandleData(ExecuteQuery(sql));
+        }
+
+        private List<Renter> HandleData(DataTable dataTable)
+        {
+            List<Renter> renters = new List<Renter>();
+            if (dataTable is null)
+                return renters;
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Renter renter = new Renter()
+                {
+                    Id = (int)row["RenterId"],
+                    Name = (string)row["Name"],
+                    RenterLevel = (RenterLevel)row["RenterLevel"]
+                };
+                renters.Add(renter);
+            }
+            return renters;
+        }
+
+        public void AddRenter(Renter newRenter)
+        {
+            string sql = $"INSERT INTO Renters VALUES('{newRenter.Name}', '{(int)newRenter.RenterLevel}')";
+            ExecuteNonQuery(sql);
+        }
+
+        public List<Renter> SearchRenters(string nameSearch)
+        {
+            string sql = $"SELECT * FROM Renters WHERE Name LIKE '%{nameSearch}%'";
+            return HandleData(ExecuteQuery(sql));
+        }
+    }
+}
---
+++ ItRental.Web/Pages/Renters.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using ItRental.Dal;
+using ItRental.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ItRental.Web.Pages
+{
+    public class RentersModel : PageModel
+    {
+        public List<Renter> Renters { get; set; }
+        [BindProperty]
+        public Renter NewRenter { get; set; }
+        [Display(Name = "Søg efter lånere")]
+        [BindProperty]
+        public string NameSearch { get; set; } = "";
+        public string MessageSearch { get; set; }
+        public string MessageAdd { get; set; } = "";
+        public RentersModel()
+        {
+            RenterRepository renterRepository = new RenterRepository();
+            Renters = renterRepository.GetRenters();
+        }
+        public void OnGet()
+        {
+         
+        }
+        public void OnPostAdd()
+        {
+            if (string.IsNullOrWhiteSpace(NewRenter.Name))
+            {
+                MessageAdd = "Navn skal udfyldes";
+            }
+            else
+            {
+                MessageAdd = "Låner oprettet";
+                RenterRepository renterRepository = new RenterRepository();
+                renterRepository.AddRenter(NewRenter);
+            }
+        }
+        public void OnPostSearch()
+        {
+            if (string.IsNullOrWhiteSpace(NameSearch))
+            {
+                MessageSearch = "Skriv noget";
+            }
+            else
+            {
+                RenterRepository renterRepository = new RenterRepository();
+                Renters = renterRepository.SearchRenters(NameSearch);
+            }
+        }
+    }
+}
